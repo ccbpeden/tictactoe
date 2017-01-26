363212e112faa5f5c5cc12ccb3d0098550394c6d
Charles Peden and Jeremiah Freeman <charles@>
2017-01-26 14:41:44 -0800

    complete basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,81 @@
-var currentPlayer,
+var clickedSpace;
+var currentUser = 'x';
+var success = false;
 
-var boardspaces = {row1 = [a, a, a], row2 = [a, a, a], row3 = [a, a, a]}
+function Freshboard(row1, row2, row3) {
+  this.row1 = row1;
+  this.row2 = row2;
+  this.row3 = row3;
+};
 
-boardSpaces.prototype.playerMark = function(){
+var cleanBoard = new Freshboard([false,false,false],[false,false,false],[false,false,false]);
 
+Freshboard.prototype.getRow = function(number){
+  if (number === 1) {
+    return this.row1;
+  } else if (number === 2) {
+    return this.row2;
+  } else {
+    return this.row3;
+  }
 }
 
-boardSpaces.prototype.validatePlay = function(){
+Freshboard.prototype.Play = function(input){
+  var rowName = input.slice(0,1);
+  rowName = "row" + rowName;
+  var colName = parseInt(input.slice(1));
+  if(this[rowName][colName] === false){
+    console.log("falsy!");
+    this[rowName][colName] = currentUser;
+    success = true;
+  } else {
+    alert("Sorry, that position has been filled.");
+  };
+};
 
+Freshboard.prototype.victoryCondition = function(){
+ if((this.row1[0] === currentUser)&&(this.row1[1] === currentUser)&&(this.row1[2] === currentUser)){
+   alert("huzzah!");
+ }else if ((this.row2[0] === currentUser)&&(this.row2[1] === currentUser)&&(this.row2[2] === currentUser)){
+   alert("huzzah!");
+ }else if ((this.row3[0] === currentUser)&&(this.row3[1] === currentUser)&&(this.row3[2] === currentUser)){
+   alert("huzzah!");
+ }else if ((this.row1[0] === currentUser)&&(this.row2[0] === currentUser)&&(this.row3[0] === currentUser)){
+   alert("huzzah!");
+ }else if ((this.row1[1] === currentUser)&&(this.row2[1] === currentUser)&&(this.row3[1] === currentUser)){
+   alert("huzzah!");
+ }else if ((this.row1[2] === currentUser)&&(this.row2[2] === currentUser)&&(this.row3[2] === currentUser)){
+   alert("huzzah!");
+ }else if ((this.row1[0] === currentUser)&&(this.row2[1] === currentUser)&&(this.row3[2] === currentUser)){
+   alert("huzzah!");
+ }else if ((this.row1[2] === currentUser)&&(this.row2[1] === currentUser)&&(this.row3[0] === currentUser)){
+   alert("huzzah!");
+ }else if ((this.row1[0] != false)&&(this.row1[1] != false)&&(this.row1[2] != false)&&(this.row2[0] != false)&&(this.row2[1] != false)&&(this.row2[2] != false)&&(this.row3[0] != false)&&(this.row3[1] != false)&&(this.row3[2] != false)){
+   alert("draw!");
+ };
+};
+
+Freshboard.prototype.turnChanger = function (){
+  if(success === true){
+    if(currentUser === "x"){
+      currentUser = "o";
+      $('#player').text(currentUser);
+    } else {
+      currentUser = "x";
+      $('#player').text(currentUser);
+    }
+  }
 }
 
-victoryCondition = function(){
-
-}
-
-turnChanger = function (){
-
-}
+$(document).ready(function() {
+  $('#player').text(currentUser);
+  $(".tic").click(function() {
+    clickedSpace = $(this).val();
+    cleanBoard.Play(clickedSpace);
+    if (success === true){
+      $(this).text(currentUser);
+      cleanBoard.victoryCondition();
+      cleanBoard.turnChanger();
+    };
+  });
+});
